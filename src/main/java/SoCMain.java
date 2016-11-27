86b916db38fe52b3f2e0ccc4bc484269e1a382c6
hittecd <hittecd@gmail.com>
2016-11-27 16:36:21 -0700

    updated main class

--- src/main/java/SoCMain.java
+++ src/main/java/SoCMain.java
@@ -1,10 +1,8 @@
-/**
- * Created by hitte on 11/27/16.
- */
+
+
 public class SoCMain {
-    private static GameManager gameManager;
 
     public static void main(String[] args) {
-        gameManager = new GameManager();
+        GameManager gameManager = new GameManager();
     }
 }
