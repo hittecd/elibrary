6759bd8be34ad57d7cd81436ae234dc3f50513c9
hittecd <hittecd@gmail.com>
2016-11-27 16:37:24 -0700

    implemented enum types

--- src/main/java/DevelopmentCard.java
+++ src/main/java/DevelopmentCard.java
@@ -1,4 +1,9 @@
 
 
 public enum DevelopmentCard {
+    KNIGHT,
+    YEAR_OF_PLENTY,
+    MONOPOLY,
+    ROAD_BUILDER,
+    VICTORY_POINT
 }
--- src/main/java/ResourceType.java
+++ src/main/java/ResourceType.java
@@ -1,4 +1,9 @@
 
 
 public enum ResourceType {
+    LUMBER,
+    SHEEP,
+    ORE,
+    WHEAT,
+    BRICK
 }
