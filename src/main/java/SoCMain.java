79c5ca7d49a7d920b7c73273b20dcbf6afba0e52
hittecd <hittecd@gmail.com>
2016-11-27 16:35:23 -0700

    created new main class: SoC.java

---
+++ src/main/java/SoCMain.java
@@ -0,0 +1,10 @@
+/**
+ * Created by hitte on 11/27/16.
+ */
+public class SoCMain {
+    private static GameManager gameManager;
+
+    public static void main(String[] args) {
+        gameManager = new GameManager();
+    }
+}
