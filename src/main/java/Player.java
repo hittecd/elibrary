4977300830e44d93bf3916ee12798724186e852b
hittecd <hittecd@gmail.com>
2016-11-28 16:53:47 -0700

    implemented Player class

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,4 +1,65 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class Player {
+
+    private final Map<ResourceType, Integer> resourceCards = new HashMap();
+    private final Map<DevelopmentCard, Integer> developmentCards = new HashMap();
+    private final List<Edge> edgesList = new ArrayList();
+    private final List<Corner> cornersList = new ArrayList();
+
+    int playerId;
+    int victoryPoints = 0;
+
+    public Player(int playerId) {
+        this.playerId = playerId;
+
+        initResourceCards();
+        initDevelopmentCards();
+    }
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public int getVictoryPoints() {
+        return victoryPoints;
+    }
+
+    private void initResourceCards() {
+        resourceCards.put(ResourceType.SHEEP, 0);
+        resourceCards.put(ResourceType.WHEAT, 0);
+        resourceCards.put(ResourceType.ORE, 0);
+        resourceCards.put(ResourceType.LUMBER, 0);
+        resourceCards.put(ResourceType.BRICK, 0);
+    }
+
+    public int getResourceCount(ResourceType resourceType) {
+        return resourceCards.get(resourceType);
+    }
+
+    public void addResourceCards(Map<ResourceType, Integer> newResourceCards) {
+
+    }
+
+    public void spendResourceCards(Map<ResourceType, Integer> spentResourceCards) {
+
+    }
+
+    private void initDevelopmentCards() {
+        developmentCards.put(DevelopmentCard.KNIGHT, 0);
+        developmentCards.put(DevelopmentCard.YEAR_OF_PLENTY, 0);
+        developmentCards.put(DevelopmentCard.ROAD_BUILDER, 0);
+        developmentCards.put(DevelopmentCard.MONOPOLY, 0);
+        developmentCards.put(DevelopmentCard.VICTORY_POINT, 0);
+    }
+
+    private void playDevelopmentCard(DevelopmentCard devCard) {
+
+    }
+
+
 }
--- src/main/java/PlayerManager.java
+++ src/main/java/PlayerManager.java
@@ -1,4 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class PlayerManager {
+
+    private int numPlayers;
+    private final List<Player> playersList = new ArrayList();
+    private int currentPlayerIndex;
+    private Player currentPlayer;
+
+    public PlayerManager(int numPlayers) {
+        this.numPlayers = numPlayers;
+
+        for(int i=0; i<numPlayers; i++) {
+            playersList.add(new Player(i+1));
+        }
+
+        currentPlayerIndex = 0;
+        currentPlayer = playersList.get(currentPlayerIndex);
+    }
+
+    public Player getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public void updateCurrentPlayer() {
+        currentPlayerIndex++;
+
+        // if the adjusted player index is greater than the number of players
+        // reset (wrap) the player index
+        if((currentPlayerIndex + 1) > numPlayers)
+            currentPlayerIndex = 0;
+    }
+
+    /**
+     * TODO: implement retrieving player by id
+     *
+     * getPlayerById(int playerId)
+     */
 }
