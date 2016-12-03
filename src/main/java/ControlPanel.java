6dd11300b07b26da02f974dd4e6b3566c133a05e
shadow9692 <shadow9692@gmail.com>
2016-12-02 21:55:56 -0700

    Changed how the GameUI worked. Set up JPanels
    GameUI should be setup now, we need to attach buttons and listeners now, but for the most part it's setup

--- src/main/java/ControlPanel.java
+++ src/main/java/ControlPanel.java
@@ -1,7 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+public class ControlPanel extends JPanel{
 
-public class ControlPanel {
+    private final int WIDTH = 390;
+    private final int HEIGHT = 790;
 
     public interface ControlPanelListener {};
 
+    public ControlPanel() {
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    }
 }
--- src/main/java/ResourcePanel.java
+++ src/main/java/ResourcePanel.java
@@ -1,7 +1,22 @@
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Line2D;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.swing.*;
 
+public class ResourcePanel extends JPanel{
 
-public class ResourcePanel {
+    private final int WIDTH = 790;
+    private final int HEIGHT = 190;
 
     public interface ResourcePanelInterface {};
 
+    public ResourcePanel() {
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+    }
 }
